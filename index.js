36056ef0f94f6ebba4fc37e1b8f703987e7c2003
Van Nguyen <nvtanh1011@gmail.com>
2023-07-21 23:43:23 +1000

    loop lab completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let messages = [];
+let names = ["Guadalupe", "Ollie","Aki"]
+
+
+function writeCards(names, eventName){
+for (let i =0; i < names.length; i++)
+{
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+}
+return  messages
+}
+//writeCards(names, "sprise")
+function countDown(number){
+for (let i =0; i < 11; i++)
+{
+    console.log(i)
+}
+}
